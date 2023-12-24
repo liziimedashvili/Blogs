a12db87e1b387ec0f44d27807b2e391916b914d2
unknown <you@example.com>
2023-12-24 05:15:26 +0400

    blog card category change

--- src/components/blogCards/BlogCard.jsx
+++ src/components/blogCards/BlogCard.jsx
@@ -1,14 +1,17 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from "react";
 import ArrowIcon from "../icons/ArrowIcon";
+import Category from "../categories/Category"; 
+import { Link } from "react-router-dom"; 
 
 export default function BlogCard({
+  id,
   image,
+  title,
   author,
   publish_date,
-  title,
-  categories,
+  categories, 
   description,
 }) {
   return (
@@ -18,7 +21,7 @@ export default function BlogCard({
           <img
             src={image}
             alt="ProductCardPhoto"
-            className=" h-[328px] w-[408px] border rounded-[12px] "
+            className="h-[328px] w-[408px] border rounded-[12px]"
           />
         </div>
         <div className="flex flex-col items-start gap-[16px]">
@@ -31,34 +34,31 @@ export default function BlogCard({
           <div className="text-[20px] text-[#1A1A1F] leading-[28px] ">
             {title}
           </div>
-          <div className="flex">
+          <div className="flex flex-wrap gap-1">
             {categories.map((category) => (
-              <div
-                key={category.id}
-                className="mr-2 px-2 py-1 text-white rounded"
-                style={{
-                  backgroundColor: category.background_color,
-                  color: category.text_color,
-                }}
-              >
-                {category.name}
-              </div>
+              <span key={category.id} className="text-xs font-medium not-italic py-[6px] px-[10px] rounded-[30px] cursor-pointer"
+              style={{
+                backgroundColor: category.background_color,
+                color: category.text_color,
+              }}
+              >{category.title}</span>
             ))}
           </div>
           <div className="font-normal text-[16px] leading-7 overflow-hidden two-line-truncate">
             {description}
           </div>
-
-          <div>
-            <button className="flex flex-row items-center">
-              <h2 className="text-[#5D37F3] font-medium leading-4 text-[14px]">
-                სრულად ნახვა{" "}
-              </h2>
-              <ArrowIcon />
-            </button>
-          </div>
+          <Link
+            to={`/blog/${id}`}
+            className="flex flex-row items-center mt-auto"
+          >
+            <h2 className="text-[#5D37F3] font-medium leading-4 text-[14px]">
+              სრულად ნახვა
+            </h2>
+            <ArrowIcon />
+          </Link>
         </div>
       </div>
     </div>
   );
+  
 }
