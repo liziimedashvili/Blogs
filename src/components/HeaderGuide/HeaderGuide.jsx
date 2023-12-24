8f20a1626b32d7071300c886d67acc90b6f93ab9
unknown <you@example.com>
2023-12-24 05:10:32 +0400

    home page guide style change

--- src/components/HeaderGuide/HeaderGuide.jsx
+++ src/components/HeaderGuide/HeaderGuide.jsx
@@ -1,17 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import MainBlogPhoto from "../../assets/BlogPhoto.png"
+import React from "react";
+import MainBlogPhoto from "../../assets/BlogPhoto.png";
 
 export default function HeaderGuide() {
   return (
-    <div className='custom-container'>
-    <div className='py-[64px] flex flex-row items-center justify-between'>
-      <div>
-        <h1 className='text-6xl font-bold leading-16 ml-5'>ბლოგი</h1>
+    <div className="custom-container">
+      <div className="py-[64px] flex flex-row items-center justify-between">
+        <div>
+          <h1 className="text-6xl font-bold leading-16 ml-5">ბლოგი</h1>
+        </div>
+        <div>
+          <img
+            src={MainBlogPhoto}
+            alt="Logo"
+            className=" w-[624px] h-[200px] "
+          />
+        </div>
       </div>
-      <div>
-      <img src={MainBlogPhoto} alt="Logo" className=" w-[624px] h-[200px] " />
-      </div>
-    </div></div>
-  )
+    </div>
+  );
 }
