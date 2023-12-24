c2713be04824a0dd0f665c0c59644acc97146cb9
unknown <you@example.com>
2023-12-24 05:07:18 +0400

    home page navigation from header with router

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,16 +1,17 @@
 import HeaderLogo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="bg-[#FFFFFF] border-b border-[#E4E3EB]">
       <header className="custom-container flex flex-row py-7 justify-between items-center ">
+      <Link to="/">
+    <img src={HeaderLogo} alt="Logo" className="w-[150px] h-[24px]" />
+  </Link>
         <div>
-          <img src={HeaderLogo} alt="Logo" className=" w-[150px] h-[24px] " />
-        </div>
-        <div>
-          <button className="py-[10px] px-[20px] bg-[#5D37F3] border rounded-[8px] text-white">
-            შესვლა
-          </button>
+            <button className="py-[10px] px-[20px] bg-[#5D37F3] border rounded-[8px] text-white">
+              შესვლა
+            </button>
         </div>
       </header>
     </div>
